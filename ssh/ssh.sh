6c7249bed634c66c118d81a7ec1b37a527d3a742
kirainl <kirain.lee@hotmail.com>
2019-06-04 19:17:47 +0800

    test commit

---
+++ ssh/ssh.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/expect -f
+# 设置ssh连接的用户名
+set user admin
+# 设置ssh连接的host地址
+set host 47.93.18.71
+# 设置ssh连接的port端口号
+set port 22
+# 设置ssh连接的登录密码
+set password 123456
+# 设置ssh连接的超时时间
+set timeout -1
+ 
+spawn ssh $user@$host -p $port
+expect "*password:"
+# 提交密码
+send "$password\r"
+# 控制权移交
+interact
+
